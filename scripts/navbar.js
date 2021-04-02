ce8fc504852fced4df30f8f5015275c912b4d911
juliaaminzadeh <juliaa3@live.unc.edu>
2021-04-01 20:21:12 -0400

    hyperlinks

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,7 +2,7 @@ var button = document.querySelector("#toggle");
 var items = document.querySelector("#items");
 
 function toggle() {
-    items.classList.toggle("active");
+    items.classList.toggle("active1");
 }
 button.addEventListener("click", toggle);
 
