01aa17a994e9eeca57680cd874be4783869b8978
Andrew Sipes <andrew.n.sipes@gmail.com>
2021-04-10 16:46:03 -0400

    Split JavaScript into separate files.

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -5,18 +5,3 @@ function toggle() {
     items.classList.toggle("active1");
 }
 button.addEventListener("click", toggle);
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
